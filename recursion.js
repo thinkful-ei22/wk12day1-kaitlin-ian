ca3ebaa6dd038a393d63b521529afad0687a8122
Ian Beihl <ianbeihl@gmail.com>
2018-08-27 16:34:21 -0700

    forgot save

--- recursion.js
+++ recursion.js
@@ -11,7 +11,7 @@ const countingSheep = num => {
 countingSheep(3);
 //iterative
 const countingSheep = num => {
-    for(let i = num; i > 0; i--){
+    for (let i = num; i > 0; i--) {
         console.log(`${i} - Another sheep jumps over the fence`);
     }
 }
@@ -30,8 +30,8 @@ doubleArray([1, 2, 3]);
 //iterative
 const doubleArray = array => {
     let answer = [];
-    for(let i = 0; i < array.length; i++){
-        answer.push(array[i] * 2 )
+    for (let i = 0; i < array.length; i++) {
+        answer.push(array[i] * 2)
     }
     return answer;
 }
@@ -52,7 +52,7 @@ reverseString('hello');
 //iterative
 const reverseString = str => {
     let answer = [];
-    for(let i = str.length - 1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         answer.push(str[i])
     }
     return answer.join('');
@@ -71,7 +71,7 @@ nTriangle(4);
 //iterative
 const nTriangle = n => {
     let sum = 0;
-    for(let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         sum += i;
     }
     return sum;
@@ -93,7 +93,7 @@ const split = (str, sep) => {
     let answer = [];
 
     let idx = str.indexOf(sep);
-    for(let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
         if (str[i] === sep) {
             answer.push(str.slice(0, idx));
             answer.push(str.slice(idx + 1, str.length));
@@ -107,7 +107,7 @@ split('hello-hello', '-');
 const binaryRep = (input) => {
     // base case 
     if (input <= 0) {
-    return '';
+        return '';
     }
     const binaryString = Math.floor(input % 2);
     // has to go from bottom to top 
@@ -119,9 +119,9 @@ binaryRep(3);
 
 const binaryRep = input => {
     let binaryString = '';
-    while(input > 0 ) {
+    while (input > 0) {
         binaryString = binaryString + Math.floor(i % 2);
-        input = Math.floor(input/2)
+        input = Math.floor(input / 2)
     }
     return binaryString;
 }
@@ -139,7 +139,7 @@ factorial(5);
 //iterative
 const factorial = num => {
     let product = 1;
-    for(let i = 1; i <= num; i++){
+    for (let i = 1; i <= num; i++) {
         product *= i;
     }
     return product;
@@ -158,8 +158,8 @@ fibonacci(7);
 //iterative
 const fibonacci = n => {
     let arr = [0, 1];
-    for (let i = 2; i < n + 1; i++){
-        arr.push(arr[i - 2] + arr[i -1])
+    for (let i = 2; i < n + 1; i++) {
+        arr.push(arr[i - 2] + arr[i - 1])
     }
     return arr[n];
 }
@@ -167,42 +167,42 @@ fibonacci(7);
 
 //anagrams---------------------
 function anagrams(prefix, str) {
-   if(str.length <= 1) {
-   console.log(`The anagram is ${prefix}${str}`);
-    4	    } else {
-    5	        for(let i = 0; i<str.length; i++) {
-    6	            let current = str.substring(i, i+1);
-    7	            let previous = str.substring(0,i);
-    8	            let after = str.substring(i+1);
-    9	            anagrams(prefix+current, previous + after);
-    10	        }
-    11	    }
-    12	}
-    13	
-	function printAnagram(word) {
-    anagrams(' ', word);
-	}
+    if (str.length <= 1) {
+        console.log(`The anagram is ${prefix}${str}`);
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            let current = str.substring(i, i + 1);
+            let previous = str.substring(0, i);
+            let after = str.substring(i + 1);
+            anagrams(prefix + current, previous + after);
+        }
+    }
+}
 
-	printAnagram('east');
+function printAnagram(word) {
+    anagrams(' ', word);
+}
+
+printAnagram('east');
 
 //animal hierarchy-------------------------
 
 const animalHierarchy = [
-    {id: 'Animals', parent: null},
-    {id: 'Mammals', parent: 'Animals'},
-    {id: 'Dogs', parent:'Mammals' },
-    {id: 'Cats', parent:'Mammals' },
-    {id: 'Golden Retriever', parent: 'Dogs'},
-    {id: 'Husky', parent:'Dogs' },
-    {id: 'Bengal', parent:'Cats' }
+    { id: 'Animals', parent: null },
+    { id: 'Mammals', parent: 'Animals' },
+    { id: 'Dogs', parent: 'Mammals' },
+    { id: 'Cats', parent: 'Mammals' },
+    { id: 'Golden Retriever', parent: 'Dogs' },
+    { id: 'Husky', parent: 'Dogs' },
+    { id: 'Bengal', parent: 'Cats' }
 ]
 
 // ==============================
 function traverse(animalHierarchy, parent) {
     let node = {};
     animalHierarchy.filter(item => item.parent === parent)
-                   .forEach(item => node[item.id] = traverse(animalHierarchy, item.id));
-    return node;  
+        .forEach(item => node[item.id] = traverse(animalHierarchy, item.id));
+    return node;
 }
 console.log(traverse(animalHierarchy, null));
 
